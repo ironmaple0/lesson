b5d51e0095a7dd8529eb7f33d12e8b168b92f3f3
ironmaple0 <89458563+ironmaple0@users.noreply.github.com>
2021-09-16 19:36:29 +0900

    fourth

---
+++ check.js
@@ -0,0 +1,8 @@
+function check() {
+            if(otoiawase.name.value == "") 
+                alert("名前を入力してください");
+              if(otoiawase.sex.value == "") 
+               alert("性別を選択してください");
+              if(otoiawase.naiyou.value == "") 
+               alert("内容を入力してください");
+}
